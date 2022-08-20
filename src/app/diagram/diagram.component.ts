100e084338c9606dddbb05c1681fd9199ff1cb81
npapp-dev <npapp86@gmail.com>
2022-08-20 21:36:16 +0200

    repo init

---
+++ src/app/diagram/diagram.component.ts
@@ -0,0 +1,101 @@
+import {
+  AfterContentInit,
+  Component,
+  ElementRef,
+  Input,
+  OnChanges,
+  OnDestroy,
+  Output,
+  ViewChild,
+  SimpleChanges,
+  EventEmitter
+} from '@angular/core';
+import {
+  BpmnPropertiesPanelModule,
+  BpmnPropertiesProviderModule,
+} from 'bpmn-js-properties-panel';
+import Modeler from 'bpmn-js/lib/Modeler';
+
+import { HttpClient } from '@angular/common/http';
+import { map, switchMap } from 'rxjs/operators';
+
+/**
+ * You may include a different variant of BpmnJS:
+ *
+ * bpmn-viewer  - displays BPMN diagrams without the ability
+ *                to navigate them
+ * bpmn-modeler - bootstraps a full-fledged BPMN editor
+ */
+const BpmnJS = require('bpmn-js/dist/bpmn-modeler.production.min.js');
+
+import { from, Observable, Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-diagram',
+  templateUrl: 'diagram.component.html',
+  styleUrls: [
+   'diagram.component.css'
+  ]
+})
+export class DiagramComponent implements AfterContentInit, OnDestroy {
+
+  // instantiate BpmnJS with component
+  private bpmnJS: Modeler;
+
+  // retrieve DOM element reference
+  @ViewChild('diagramRef', { static: true }) private diagramRef: ElementRef | undefined;
+
+  @ViewChild('propertiesRef', { static: true }) private propertiesRef: ElementRef | undefined;
+
+  private xml: string = `<?xml version="1.0" encoding="UTF-8"?>
+  <bpmn2:definitions xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:bpmn2="http://www.omg.org/spec/BPMN/20100524/MODEL" xmlns:bpmndi="http://www.omg.org/spec/BPMN/20100524/DI" xmlns:dc="http://www.omg.org/spec/DD/20100524/DC" xmlns:di="http://www.omg.org/spec/DD/20100524/DI" xsi:schemaLocation="http://www.omg.org/spec/BPMN/20100524/MODEL BPMN20.xsd" id="sample-diagram" targetNamespace="http://bpmn.io/schema/bpmn">
+    <bpmn2:process id="Process_1" isExecutable="false">
+      <bpmn2:startEvent id="StartEvent_1"/>
+    </bpmn2:process>
+    <bpmndi:BPMNDiagram id="BPMNDiagram_1">
+      <bpmndi:BPMNPlane id="BPMNPlane_1" bpmnElement="Process_1">
+        <bpmndi:BPMNShape id="_BPMNShape_StartEvent_2" bpmnElement="StartEvent_1">
+          <dc:Bounds height="36.0" width="36.0" x="412.0" y="240.0"/>
+        </bpmndi:BPMNShape>
+      </bpmndi:BPMNPlane>
+    </bpmndi:BPMNDiagram>
+  </bpmn2:definitions>`;
+
+  constructor(private http: HttpClient) {
+
+    this.bpmnJS = new Modeler({
+      container: this.diagramRef,
+      propertiesPanel: {
+        parent: this.propertiesRef
+      },
+      additionalModules: [
+        BpmnPropertiesPanelModule,
+        BpmnPropertiesProviderModule
+      ]
+    })
+  }
+
+  ngAfterContentInit(): void {
+    // attach BpmnJS instance to DOM element
+    this.bpmnJS.attachTo(this.diagramRef!.nativeElement);
+  
+    const propertiesPanel =this.bpmnJS.get('propertiesPanel');
+
+    propertiesPanel.attachTo(this.propertiesRef!.nativeElement);
+    this.importDiagram(this.xml);
+  }
+
+
+  ngOnDestroy(): void {
+    // destroy BpmnJS instance
+    this.bpmnJS.destroy();
+  }
+
+  /**
+   * Creates a Promise to import the given XML into the current
+   * BpmnJS instance, then returns it as an Observable.
+   */
+  private importDiagram(xml: string): Observable<{warnings: Array<any>}> {
+    return from(this.bpmnJS.importXML(xml) as Promise<{warnings: Array<any>}>);
+  }
+}
