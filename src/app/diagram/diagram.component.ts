0343b5810ce17503bf899f77fc6d8a5f387309a2
npapp-dev <npapp86@gmail.com>
2025-02-24 22:10:29 +0100

    Add diagram export functionality with save button
    Introduced a method to export the BPMN diagram as an XML file and trigger its download. Added a "Save Diagram" button in the UI, styled with a new `.save-button` class, to enable users to trigger the export action easily. Adjusted lifecycle hooks to switch from `AfterContentInit` to `AfterViewInit` for proper initialization.

--- src/app/diagram/diagram.component.ts
+++ src/app/diagram/diagram.component.ts
@@ -1,4 +1,4 @@
-import {AfterContentInit, Component, ElementRef, OnDestroy, ViewChild} from '@angular/core';
+import {AfterContentInit, AfterViewInit, Component, ElementRef, OnDestroy, ViewChild} from '@angular/core';
 import {BpmnPropertiesPanelModule, BpmnPropertiesProviderModule,} from 'bpmn-js-properties-panel';
 import Modeler from 'bpmn-js/lib/Modeler';
 import customPropertiesProvider from '../custom-properties-provider/custom-property-provider';
@@ -16,21 +16,21 @@ const custom = require('../custom-properties-provider/descriptors/custom.json');
 const BpmnJS = require('bpmn-js/dist/bpmn-modeler.production.min.js');
 
 @Component({
-    selector: 'app-diagram',
-    templateUrl: 'diagram.component.html',
-    styleUrls: [
-        'diagram.component.css'
-    ],
-    standalone: false
+  selector: 'app-diagram',
+  templateUrl: 'diagram.component.html',
+  styleUrls: [
+    'diagram.component.css'
+  ],
+  standalone: false
 })
-export class DiagramComponent implements AfterContentInit, OnDestroy {
+export class DiagramComponent implements AfterViewInit, OnDestroy {
 
   // instantiate BpmnJS with component
-  private bpmnJS: Modeler;
+  readonly bpmnJS: Modeler;
 
   // retrieve DOM element reference
-  @ViewChild('diagramRef', { static: true }) private diagramRef: ElementRef | undefined;
-  @ViewChild('propertiesRef', { static: true }) private propertiesRef: ElementRef | undefined;
+  @ViewChild('diagramRef', {static: true}) diagramRef: ElementRef | undefined;
+  @ViewChild('propertiesRef', {static: true}) propertiesRef: ElementRef | undefined;
 
   private xml: string = `<?xml version="1.0" encoding="UTF-8"?>
   <bpmn2:definitions xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:bpmn2="http://www.omg.org/spec/BPMN/20100524/MODEL" xmlns:bpmndi="http://www.omg.org/spec/BPMN/20100524/DI" xmlns:dc="http://www.omg.org/spec/DD/20100524/DC" xmlns:di="http://www.omg.org/spec/DD/20100524/DI" xsi:schemaLocation="http://www.omg.org/spec/BPMN/20100524/MODEL BPMN20.xsd" id="sample-diagram" targetNamespace="http://bpmn.io/schema/bpmn">
@@ -63,28 +63,63 @@ export class DiagramComponent implements AfterContentInit, OnDestroy {
     })
   }
 
-  ngAfterContentInit(): void {
+  ngAfterViewInit(): void {
     // attach BpmnJS instance to DOM element
     this.bpmnJS.attachTo(this.diagramRef!.nativeElement);
 
-    const propertiesPanel =this.bpmnJS.get('propertiesPanel');
+    const propertiesPanel = this.bpmnJS.get('propertiesPanel');
 
     // @ts-ignore
     propertiesPanel.attachTo(this.propertiesRef!.nativeElement);
     this.importDiagram(this.xml);
   }
 
-
-  ngOnDestroy(): void {
-    // destroy BpmnJS instance
-    this.bpmnJS.destroy();
-  }
-
   /**
    * Creates a Promise to import the given XML into the current
    * BpmnJS instance, then returns it as an Observable.
    */
-  private importDiagram(xml: string): Observable<{warnings: Array<any>}> {
-    return from(this.bpmnJS.importXML(xml) as Promise<{warnings: Array<any>}>);
+  private importDiagram(xml: string): Observable<{ warnings: Array<any> }> {
+    return from(this.bpmnJS.importXML(xml) as Promise<{ warnings: Array<any> }>);
+  }
+
+  /**
+   * Exports the current BPMN diagram as an XML file and triggers a download of the file.
+   * The exported diagram will have a `.xml` file extension and include proper formatting.
+   * Shows an error in the console if the `bpmnJS` instance is not initialized.
+   *
+   * @return {void} No value is returned. The function performs file export and download actions.
+   */
+  exportDiagram(): void {
+    if (!this.bpmnJS) {
+      console.error('bpmnJS is not initialized');
+      return;
+    }
+
+    this.bpmnJS.saveXML({format: true}).then((xml) => {
+      // Create a Blob from the XML data
+      const blob = new Blob([xml.xml ?? ''], {type: 'text/xml'});
+      const url = window.URL.createObjectURL(blob);
+
+      // Create a download link
+      const a = document.createElement('a');
+      a.href = url;
+      a.download = 'diagram.xml';
+      a.style.display = 'none';
+
+      // Append the link, trigger download, and clean up
+      document.body.appendChild(a);
+      a.click();
+      document.body.removeChild(a);
+
+      // Revoke the object URL after a short delay
+      setTimeout(() => window.URL.revokeObjectURL(url), 100);
+    }).catch((err) => {
+      console.log(err);
+    });
+  }
+
+  ngOnDestroy(): void {
+    // destroy BpmnJS instance
+    this.bpmnJS.destroy();
   }
 }
